6250baef66dcfc502aef3d57cca654b5a6aea148
shandilyaaryan <aryanshandilya10@gmail.com>
2025-04-19 08:00:54 +0000

    Added common middlewares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config({
-    path: "./.env"
+    path: "./.env",
 });
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`);
+});
