8ab44c013901aabe3d8d3a61233dafaa16974ad0
shandilyaaryan <aryanshandilya10@gmail.com>
2025-04-19 07:51:40 +0000

    Basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Aryan Shandilya");
+import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+})
